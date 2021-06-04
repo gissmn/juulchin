c8af0685fd1ba5f9658f857ce4fefc88eee63d80
Ganzorig.B <gissmn@gmail.com>
2021-06-04 19:34:07 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="navbar">
+        <img src={logo} className="logo" alt="logo" />
+        <div id="navMenu">
+          <ul className="menu">
+            <li>Tours</li>
+            <li>Monthly Special tours</li>
+            <li>Festival & Events</li>
+            <li>Tailor Made</li>
+            <li>Blog</li>
+            <li>Log in</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
